fd2b0ded88213e31535b17b1a333bce3b6c1f5e5
joaquimgarcia <joaquimdgarcia@gmail.com>
2016-03-05 02:47:46 -0300

    ok

--- src/xprs_env.jl
+++ src/xprs_env.jl
@@ -7,13 +7,9 @@ type Env
     function Env()
         ret = @xprs_ccall(init, Cint, (Ptr{Cchar},), C_NULL)
         if ret != 0
-            if ret == 10009
-                error("Invalid Xpress license")
-            else
-                error("Failed to create environment (error $ret).")
-            end
+            error("Failed to create environment (error $ret).")
         end
-        env = new(1)
+        env = ret
         # finalizer(env, free_env)  ## temporary disable: which tends to sometimes caused warnings
         env
     end
@@ -22,29 +18,33 @@ end
 #Base.unsafe_convert(ty::Type{Ptr{Void}}, env::Env) = env.ptr_env::Ptr{Void}
 
 function is_valid(env::Env)
-    env.ptr_env == 1
+    env.ptr_env == 0
 end
 
 function free_env(env::Env)
-    if env.ptr_env != 0
-        ret = @xprs_ccall(free, Cint, ())
+    if env.ptr_env == C_NULL
+        ret = @xprs_ccall(free, Cint, (Void,))
+        if ret != 0
+            error("Failed to free environment (error $ret).")
+        end
         env.ptr_env = 0
     end
 end
 
-# function get_error_msg(env::Env)
-#     @assert env.ptr_env == 1
-#     sz = @xprs_ccall(geterrormsg, Ptr{UInt8}, (Ptr{Void},), env.ptr_env)
-# end
-# 
-# # error
-# 
-# type XpressError
-#     code::Int
-#     msg::ASCIIString 
-#     
-#     function XpressError(env::Env, code::Integer)
-#         new(convert(Int, code), get_error_msg(env))
-#     end
-# end
+function get_error_msg(env::Env)
+    @assert env.ptr_env == 0
+    sz = @xprs_ccall(geterrormsg, Ptr{UInt8}, (Ptr{Void},), env.ptr_env)
+    bytestring(sz)
+end
+
+# error
+
+type XpressError
+    code::Int
+    msg::ASCIIString 
+    
+    function XpressError(env::Env, code::Integer)
+        new(convert(Int, code), get_error_msg(env))
+    end
+end
 
