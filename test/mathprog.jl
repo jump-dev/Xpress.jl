3f26aaa2613c67121d064a773f448d308239523a
joaquimgarcia_psr <joaquimgarcia@psr-inc.com>
2016-04-24 16:24:08 -0300

    update mathprog tests

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -1,20 +1,26 @@
 using Xpress
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
-linprogtest(XpressSolver())
-
 include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
+println("Testing linproginterface with solver Xpress.XpressSolver")
 linprogsolvertest(XpressSolver())
+println("Done")
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
 mixintprogtest(XpressSolver())
 
 
-#include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
-#quadprogtest(XpressSolver())
-#socptest(XpressSolver())
-#qpdualtest(XpressSolver(QCPDual=1))
+include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
+#quadprogtest(XpressSolver()) #fails
+socptest(XpressSolver())
+#qpdualtest(XpressSolver()) #fails
 
-#include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
-#coniclineartest(XpressSolver())
-#conicSOCtest(XpressSolver())
+include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
+println("Testing coniclineartest with solver Xpress.XpressSolver")
+coniclineartest(XpressSolver())
+println("Done")
+println("Testing conicSOCtest with solver Xpress.XpressSolver")
+conicSOCtest(XpressSolver())
+println("Done")
+
+include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
+linprogtest(XpressSolver()) #fails on rays
