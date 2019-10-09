01867057b0a7e6e57b0631aba7fb6d7a84860c07
Jose Daniel Lara <jdlara@berkeley.edu>
2019-10-09 11:38:43 -0700

    update Environment definition

--- src/xprs_env.jl
+++ src/xprs_env.jl
@@ -5,16 +5,23 @@
 Initialize Xpress environment.
 Need to do ir once and only once.
 """
-function Env()
-    ret = @xprs_ccall(init, Cint, (Ptr{Cchar},), C_NULL)
-    if ret != 0
-        if ret == :number
-            error("Invalid Xpress license")
-        else
-            error("Failed to create environment (error $ret).")
+mutable struct Env
+    ptr_env::Ptr{Cvoid}
+
+    function Env()
+        a = Ref{Ptr{Cvoid}}()
+        ret = @xprs_ccall(init, Cint, (Ptr{Cchar},), C_NULL)
+        if ret != 0
+            if ret == :number
+                error("Invalid Xpress license")
+            else
+                error("Failed to create environment (error $ret).")
+            end
         end
+        env = new(a[])
+        # finalizer(env, free_env)  ## temporary disable: which tends to sometimes caused warnings
+        env
     end
-    # finalizer(env, free_env)  ## temporary disable: which tends to sometimes caused warnings
 end
 
 function is_valid()
@@ -27,6 +34,34 @@ end
 
 Finalize Xpress environment.
 """
-function free_env()
-    ret = @xprs_ccall(free, Cint, ())
+function free_env(env::Env)
+    if env.ptr_env != C_NULL
+        ret = @xprs_ccall(free, Cint, ())
+        env.ptr_env = C_NULL
+    end
 end
+
+Base.unsafe_convert(ty::Type{Ptr{Cvoid}}, env::Env) = env.ptr_env::Ptr{Cvoid}
+
+function is_valid(env::Env)
+    env.ptr_env != C_NULL
+end
+
+#= error
+function get_error_msg(env::Env)
+    @assert env.ptr_env != C_NULL
+    sz = @grb_ccall(geterrormsg, Ptr{UInt8}, (Ptr{Cvoid},), env.ptr_env)
+    unsafe_string(sz)
+end
+
+
+
+mutable struct XpressError <: Exception
+    code::Int
+    msg::String
+
+    function XpressError(env::Env, code::Integer)
+        new(convert(Int, code), get_error_msg(env))
+    end
+end
+=#
