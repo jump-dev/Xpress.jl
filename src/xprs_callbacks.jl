1a12d87e9b9edb936adfed961b7b21c31562066a
rafabench <rafa.bench@gmail.com>
2022-06-16 20:11:46 -0300

    fix unsafe_convert in CallbackData

--- src/xprs_callbacks.jl
+++ src/xprs_callbacks.jl
@@ -5,7 +5,7 @@ mutable struct CallbackData
     model::XpressProblem # local model # ptr_model::Ptr{Nothing}
 end
 Base.cconvert(::Type{Ptr{Cvoid}}, x::CallbackData) = x
-Base.unsafe_convert(::Type{Ptr{Cvoid}}, x::CallbackData) = x.ptr::Ptr{Cvoid}
+Base.unsafe_convert(::Type{Ptr{Cvoid}}, x::CallbackData) = x.model.inner.ptr::Ptr{Cvoid}
 Base.broadcastable(x::CallbackData) = Ref(x)
 
 # must be mutable
