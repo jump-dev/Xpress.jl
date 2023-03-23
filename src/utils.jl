b4950e9396683e370442271e2c3611236f921051
RaphMota <raphael.mota@student-cs.fr>
2023-03-23 15:37:07 -0300

    Adding to_str to utils

--- src/utils.jl
+++ src/utils.jl
@@ -140,3 +140,78 @@ macro checked(expr)
         end
     end)
 end
+
+wrap_with_parens(x::String) = string("(", x, ")")
+
+to_str(x) = string(x)
+
+struct UnrecognizedExpressionException <: Exception
+    exprtype::String
+    expr
+end
+
+function to_str(c::Expr)
+    if c.head == :comparison
+        if length(c.args) == 3
+            return join([to_str(c.args[1]), c.args[2], c.args[3]], " ")
+        elseif length(c.args) == 5
+            return join([c.args[1], c.args[2], to_str(c.args[3]),
+                         c.args[4], c.args[5]], " ")
+        else
+            throw(UnrecognizedExpressionException("comparison", c))
+        end
+    elseif c.head == :call
+        if c.args[1] in (:<=,:>=,:(==))
+            if length(c.args) == 3
+                return join([to_str(c.args[2]), to_str(c.args[1]), to_str(c.args[3])], " ")
+            elseif length(c.args) == 5
+                return join([to_str(c.args[1]), to_str(c.args[2]), to_str(c.args[3]), to_str(c.args[4]), to_str(c.args[5])], " ")
+            else
+                throw(UnrecognizedExpressionException("comparison", c))
+            end
+        elseif c.args[1] in (:+,:-,:*,:/)
+            if all(d->isa(d, Real), c.args[2:end]) # handle unary case
+                return wrap_with_parens(string(eval(c)))
+            elseif c.args[1] == :- && length(c.args) == 2
+                return wrap_with_parens(string("(-$(to_str(c.args[2])))"))
+            else
+                return wrap_with_parens(string(join([to_str(d) for d in c.args[2:end]], string(c.args[1]))))
+            end
+        elseif c.args[1] == :^
+            if length(c.args) != 3
+                throw(UnrecognizedExpressionException("function call", c))
+            end
+            if c.args[3] isa Real
+                return wrap_with_parens(string(to_str(c.args[2]), c.args[1], c.args[3]))
+            else
+                # BARON does not support x^y natively for x,y variables. Instead
+                # we transform to the equivalent expression exp(y * log(x)).
+                return to_str(:( exp( $(c.args[3]) * log($(c.args[2])) ) ))
+            end
+        elseif c.args[1] in (:exp,:log)
+            if length(c.args) != 2
+                throw(UnrecognizedExpressionException("function call", c))
+            end
+            return wrap_with_parens(string(c.args[1], wrap_with_parens(to_str(c.args[2]))))
+        elseif c.args[1] == :abs
+            if length(c.args) != 2
+                throw(UnrecognizedExpressionException("function call", c))
+            end
+            # BARON does not support abs(x) natively for variable x. Instead
+            # we transform to the equivalent expression sqrt(x^2).
+            return to_str(:( ( $(c.args[2])^2.0 )^(0.5) ))
+        else
+            throw(UnrecognizedExpressionException("function call", c))
+        end
+    elseif c.head == :ref
+        if c.args[1] == :x
+            idx = c.args[2]
+            @assert isa(idx, Int)
+            # TODO decide is use use defined names
+            # might be messy becaus a use can call his variable "sin"
+            return "x$idx"
+        else
+            throw(UnrecognizedExpressionException("reference", c))
+        end
+    end
+end
