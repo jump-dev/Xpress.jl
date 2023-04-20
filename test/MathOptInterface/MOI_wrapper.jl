24f5736c08af3037d6302e9e4d4f39391274e6b1
RaphMota <raphael.mota@student-cs.fr>
2023-04-20 16:32:18 -0300

    Removing test_temp

--- test/MathOptInterface/MOI_wrapper.jl
+++ test/MathOptInterface/MOI_wrapper.jl
@@ -44,13 +44,6 @@ function test_Basic_Parameters()
     @test MOI.get(optimizer, MOI.SolverName()) == "Xpress"
 end
 
-function test_temp()
-    optimizer = Xpress.Optimizer(OUTPUTLOG = 0)
-    model = MOI.Bridges.full_bridge_optimizer(optimizer, Float64)
-    MOI.set(model, MOI.Silent(), true)
-    test_nonlinear_objective(model,Config(atol = 0.2, rtol = 0.2, optimal_status = MOI.LOCALLY_SOLVED))
-end
-
 function test_runtests()
 
     optimizer = Xpress.Optimizer(OUTPUTLOG = 0)
@@ -65,10 +58,6 @@ function test_runtests()
             "test_constraint_PrimalStart_DualStart_SecondOrderCone",
             "_RotatedSecondOrderCone_",
             "_GeometricMeanCone_",
-            "_nonlinear_hs071_NLPBlockDual",
-            "_nonlinear_objective",
-            "_nonlinear_objective_and_moi_objective_test",
-            "_nonlinear_without_objective",
             # Xpress cannot handle nonconvex quadratic constraint
             "test_quadratic_nonconvex_",
         ],
@@ -90,23 +79,6 @@ function test_runtests()
             "_GeometricMeanCone_",
             
         ]
-    )
-    return
-end
-
-function test_runtests_nlp()
-
-    optimizer = Xpress.Optimizer(OUTPUTLOG = 0, PRESOLVE = 0)
-    model = MOI.Bridges.full_bridge_optimizer(optimizer, Float64)
-    MOI.set(model, MOI.Silent(), true)
-    MOI.Test.runtests(
-        model,
-        MOI.Test.Config(atol = 0.2, rtol = 0.2, optimal_status = MOI.LOCALLY_SOLVED),
-        include = String[
-            # tested with PRESOLVE=0 below
-            "_nonlinear_hs071_NLPBlockDual",
-            "_nonlinear_objective",
-        ],
     )
     return
 end
