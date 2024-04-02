a78065a2097dbe9ecf9a8581f7a3ed1edb0f72e9
odow <o.dowson@gmail.com>
2024-04-03 11:58:32 +1300

    Fix getter of MOI.ListOfConstraintIndices

--- test/test_MOI_wrapper.jl
+++ test/test_MOI_wrapper.jl
@@ -2087,6 +2087,18 @@ function test_modify_different_sparsity()
     return
 end
 
+function test_ListOfConstraintIndices()
+    model = Xpress.Optimizer()
+    x = MOI.add_variable(model)
+    c = MOI.add_constraint(model, 1.0 * x[1], MOI.LessThan(0.0))
+    F, S = MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}
+    G = MOI.ScalarQuadraticFunction{Float64}
+    @test MOI.get(model, MOI.ListOfConstraintIndices{F,S}()) == [c]
+    @test isempty(MOI.get(model, MOI.ListOfConstraintIndices{G,S}()))
+    return
+end
+
+
 function test_ListOfConstraintTypesPresent()
     model = Xpress.Optimizer()
     sets = (
