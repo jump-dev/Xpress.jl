0518fdf50af04a49c0c7315d1522e5a62aff842d
Oscar Dowson <odow@users.noreply.github.com>
2025-09-04 16:04:24 +1200

    Update test_MOI_wrapper.jl

--- test/test_MOI_wrapper.jl
+++ test/test_MOI_wrapper.jl
@@ -58,8 +58,8 @@ function test_runtests()
         MOI.Test.Config(; atol = 1e-3, rtol = 1e-3);
         exclude = [
             # An upstream bug in Xpress@9.7.0
-            # "test_solve_conflict_bound_bound",
-            # "test_solve_conflict_invalid_interval",
+            "test_solve_conflict_bound_bound",
+            "test_solve_conflict_invalid_interval",
             # tested with PRESOLVE=0 below
             "_SecondOrderCone_",
             "test_constraint_PrimalStart_DualStart_SecondOrderCone",
